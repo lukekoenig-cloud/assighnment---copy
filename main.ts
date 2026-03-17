1e60acfbadb9535a89e5e43051d49cb7473c718b
lukekoenig-cloud <luke.koenig@marcellin.school.nz>
2026-03-18 12:24:10 +1300

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -5,20 +5,31 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
-        . . . . . . . 9 9 9 9 9 9 . . . 
-        . . . . . . . 9 9 9 9 5 9 . . . 
-        . . 9 9 9 9 9 9 9 9 5 9 9 . . . 
-        9 9 9 9 9 9 9 9 9 9 9 9 . . . . 
+        9 9 9 9 1 1 1 1 1 1 1 . . . . . 
+        f f f f 1 5 5 5 5 5 1 . . . . . 
+        f f f f 1 5 5 5 5 5 1 . . . . . 
+        f f f f 1 5 1 1 1 1 1 . . . . . 
+        9 9 9 9 1 1 1 . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `, mySprite, 200, 0)
+        `, mySprite, 50, 0)
 })
-let enemy_ship: Sprite = null
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprites.destroy(sprite)
+    sprites.destroy(otherSprite)
+    info.changeScoreBy(1)
+    scene.cameraShake(2, 500)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    sprites.destroy(otherSprite, effects.bubbles, 500)
+    scene.cameraShake(4, 500)
+})
+let enemyship: Sprite = null
 let projectile: Sprite = null
 let mySprite: Sprite = null
 effects.starField.startScreenEffect()
@@ -26,39 +37,43 @@ mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . 4 4 4 
-    . . . . . . . . . . 6 6 6 6 . . 
-    . . . . . . . . 6 6 6 6 6 6 . . 
-    . . . . . . 6 6 6 6 6 6 6 6 . . 
-    . . . 6 6 6 6 6 6 6 6 6 6 6 . . 
-    . 8 8 8 8 8 8 8 8 8 8 8 8 a . . 
-    6 6 6 6 6 6 6 6 6 6 6 6 a a a . 
-    6 6 6 6 6 6 6 6 6 6 6 6 a a a . 
-    . 8 8 8 8 8 8 8 8 8 8 8 8 a . . 
-    . . . . . 6 6 6 6 6 6 6 6 6 . . 
-    . . . . . . . . 6 6 6 6 6 6 . . 
-    . . . . . . . . . . 6 6 4 4 4 4 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . 2 2 2 2 2 2 2 . . . . . . 
+    . . 2 f f f f f f f 2 2 2 2 . . 
+    . . 2 f f f f f f f f f a f 2 . 
+    . . 2 f f f f f f f f f f f 5 . 
+    . . 2 f f f f f f f f f a f 2 . 
+    . . 2 f f f f f f f 2 2 2 2 . . 
+    . . . 2 2 2 2 2 2 2 . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
-controller.moveSprite(mySprite)
+controller.moveSprite(mySprite, 200, 200)
 mySprite.setStayInScreen(true)
-game.onUpdateInterval(2000, function () {
-    enemy_ship = sprites.create(img`
+info.setLife(2)
+game.onUpdateInterval(1000, function () {
+    enemyship = sprites.create(img`
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
-        . . . . . . 2 2 . . . . . . . . 
-        . . . . . . 3 2 2 2 . . . . . . 
-        . . . . . . 3 3 3 2 2 4 . . . . 
-        . . . . . . 2 3 3 3 2 2 . . . . 
-        . . . 2 . . 2 3 3 3 3 2 . . . . 
-        . . 2 2 2 2 2 3 3 3 3 3 2 . . . 
-        . . . . . . 2 2 2 2 2 2 2 . . . 
-        . . . . . . 2 2 2 2 2 3 3 2 . . 
-        . . . . . . 2 2 2 3 3 3 3 2 4 . 
-        . . . . . . . . 2 3 2 3 3 2 2 . 
-        . . . . . . . . . . 2 2 2 2 2 . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
-        `, SpriteKind.Player)
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . c . . . . . . . . 
+        . . . . c a a a c . . . . . . . 
+        . . . c c f a b b c . . . . . . 
+        . . . b f f b f a a . . . . . . 
+        . . . b b a b f f a . . . . . . 
+        . . . c b f b b a c . . . . . . 
+        . . . . b a f c c . . . . . . . 
+        . . . . . b b c . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    enemyship.x = scene.screenWidth()
+    enemyship.vx = -20
+    enemyship.y = randint(10, scene.screenHeight() - 10)
 })
